98166878dc8d8906b1ec28fa210818cb115d1898
Kisan Thapa <kisanthapa33@gmail.com>
2022-10-24 00:47:01 -0400

    Login page created

---
+++ src/components/buttons/forgetPasswordButton.jsx
@@ -0,0 +1,23 @@
+export function ForgetPasswordButton(props) {
+    return (
+        <button
+            onClick={props.onClick}
+            className='transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-200 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset'>
+            <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+                className='w-4 h-4 inline-block align-text-top'
+            >
+                <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth='2'
+                    d='M8 11V7a4 4 0 118 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z'
+                />
+            </svg>
+            <span className='inline-block ml-1'>Forgot Password</span>
+        </button>
+    );
+}
---
+++ src/components/primaryButton.jsx
@@ -0,0 +1,10 @@
+export function PrimaryButton(props) {
+    return (
+        <button
+            type={props.type}
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+        >
+            {props.text}
+        </button>
+    );
+}
